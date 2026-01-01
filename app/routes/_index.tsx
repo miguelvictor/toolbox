9fb30c7b76db4538cdbe83374b0bec89c0a80125
Miguel Victor <miguel@digitalfans.se>
2026-01-01 16:10:36 +0100

    feat: added shadcn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,5 @@
+import { ComponentExample } from "~/components/component-example"
+
 export default function HomePage() {
-  return (
-    <main className="p-10">
-      <h1 className="font-semibold tracking-tight text-3xl">My Toolbox</h1>
-    </main>
-  )
+  return <ComponentExample />
 }
